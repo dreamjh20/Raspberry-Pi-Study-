d7e80e3fd66e7be971d1ed0ab9f978c8ea36bac9
dreamjh20 <dreamjh20@gmail.com>
2021-07-09 11:49:55 +0900

    [UPDATE]Divisor Counter Natural Number Exeption Handling

--- Divisor/main.c
+++ Divisor/main.c
@@ -5,6 +5,7 @@ int main()
 {
 	int num;
 	int divisor = 0;
+
 	scanf("%d", &num);
 	if(num <= 0)
 	{
@@ -16,6 +17,7 @@ int main()
 		printf("ONLY NATURAL NUMBER!\n");
 		return 0;
 	}
+
 	divisor = Divisor(num);
 	Print(divisor);
 	return 0;
